1e168a39053a5a3dcaa361aa50b904ad798b0809
Андрюха <andryhamorozyuk7@gmail.com>
2022-08-19 23:14:42 +0300

    ДЗ №5

--- homework_for_lesson_No5.py
+++ homework_for_lesson_No5.py
@@ -1,21 +1,20 @@
 # Task 1
 # The Guessing Game.
-# import random
-#
-# x = random.randint(1, 10)
-# while True:
-#     s = int(input("Введіть число від 1 до 10 включно: "))
-#     if s == x:
-#         print(f"Вітаю! Ти вгадав чило {x}!")
-#         break
-#     else:
-#         print("Спробуй ще раз!")
+import random
+x = random.randint(1, 10)
+while True:
+    s = int(input("Введіть число від 1 до 10 включно: "))
+    if s == x:
+        print(f"Вітаю! Ти вгадав чило {x}!")
+        break
+    else:
+        print("Спробуй ще раз!")
 
 # Task 2
 # The birthday greeting program.
-# name = input("Ваше ім'я: ")
-# year = int(input("Ваш вік: "))
-# print(f'Привіт, {name}, на твій наступний день народження тобі виповниться {year + 1 } років')
+name = input("Ваше ім'я: ")
+year = int(input("Ваш вік: "))
+print(f'Привіт, {name}, на твій наступний день народження тобі виповниться {year + 1 } років')
 
 # Task 3
 # Words combination
