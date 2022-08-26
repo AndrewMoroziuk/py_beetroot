82b7bbc860bb2de4fbc2601d63a37c8a3d8c0582
Андрій <andryhamorozyuk7@gmail.com>
2022-08-26 16:52:51 +0300

    Додав ДЗ 9 в окремій директорії.

--- Homework Standart/homework_for_lesson_No9/download.py
+++ Homework Standart/homework_for_lesson_No9/download.py
@@ -6,9 +6,26 @@ def read_and_copy_text_by_sentence(text: str = None):
         print("Нажаль нема тут чого тут читати...")
     else:
         print('Ось текст: ', text)
+        text = text.replace('?', '?.')
         list_sentence = [s.capitalize() for s in text.split('. ')]
         if os.path.isfile('./Result/Копія.txt'):
             os.remove('./Result/Копія.txt')
         with open('./Result/Копія.txt', 'w') as file_res:
             for row in list_sentence:
                 file_res.write(row + '.\n')
+
+
+def count_row(text: str):
+    c_count = int()
+    if os.path.isfile('./Result/Копія.txt'):
+        with open('./Result/Копія.txt', 'r') as file:
+            c_rows = len(file.readlines())
+        print('Кількість рядків у файлі: ', c_rows)
+
+
+def count_chr(text: str):
+    c_count = int()
+    if os.path.isfile('./Result/Копія.txt'):
+        with open('./Result/Копія.txt', 'r') as file:
+            c_chr = len(file.read())
+        print('Кількість слів у файлі: ', c_chr)
--- Homework Standart/homework_for_lesson_No9/main.py
+++ Homework Standart/homework_for_lesson_No9/main.py
@@ -1,5 +1,8 @@
-from download import read_and_copy_text_by_sentence as copy_text
+from download import read_and_copy_text_by_sentence as copy_text, \
+    count_row, count_chr
 from Homework_Plus.data import row_for_hm_9 as row
 
 if __name__ == "__main__":
     copy_text(row)
+    count_row(row)
+    count_chr(row)
