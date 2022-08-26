2e698b75d535267c8717f6d34eded1ac91917c28
Андрій <andryhamorozyuk7@gmail.com>
2022-08-26 17:00:54 +0300

    Додав ДЗ 9 в окремій директорії.

--- Homework Standart/homework_for_lesson_No9/download.py
+++ Homework Standart/homework_for_lesson_No9/download.py
@@ -1,18 +1,25 @@
 import os
+from typing import List
 
 
 def read_and_copy_text_by_sentence(text: str = None):
+    name_res_file = 'Копія.txt'
     if text is None:
         print("Нажаль нема тут чого тут читати...")
     else:
         print('Ось текст: ', text)
-        text = text.replace('?', '?.')
+
+        for symbol in '?!;':
+            text = text.replace(symbol, str(symbol) + '.')
+
         list_sentence = [s.capitalize() for s in text.split('. ')]
-        if os.path.isfile('./Result/Копія.txt'):
-            os.remove('./Result/Копія.txt')
-        with open('./Result/Копія.txt', 'w') as file_res:
+
+        if os.path.isfile(f'./Result/{name_res_file}'):
+            os.remove(f'./Result/{name_res_file}')
+
+        with open(f'./Result/{name_res_file}', 'w') as file_res:
             for row in list_sentence:
-                file_res.write(row + '.\n')
+                file_res.write(row + '\n')
 
 
 def count_row(text: str):
