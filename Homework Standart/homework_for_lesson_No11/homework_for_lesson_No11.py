0ea7d5563c78126bad83089e19c69319c62fd784
Андрій <andryhamorozyuk7@gmail.com>
2022-08-31 23:20:18 +0300

    Додав ДЗ 9,10 в окремій директорії.

---
+++ Homework Standart/homework_for_lesson_No11/homework_for_lesson_No11.py
@@ -0,0 +1,78 @@
+import glob
+
+
+def name_files(path=''):
+    """
+    Функція для функції 'write_file'.
+    Виводить на екран список файлів з директорії.
+    """
+    list_files = {}
+    names_files = glob.glob(path + '*.txt')
+
+    if names_files is None:
+        return [None]
+
+    print('| №  |Назва файлу|\n+----+-----------+')
+    num_key = 1
+    for file in names_files:
+        list_files[num_key] = file
+        print('|', str(num_key).rjust(2), '|', file.ljust(10), '|')
+        num_key += 1
+    print('+----+-----------+')
+
+    return list_files
+
+
+def write_file():
+    """
+    Записуємо новий текстовий файл.
+    """
+    repeat_write_file = 'y'
+    while repeat_write_file == 'y':
+        file_name = input('Назва нового файлу: ')
+        text_for_write = input('Введіть текст, для запису: ')
+
+        with open(f'./{file_name}.txt', 'w') as my_file:
+            my_file.write(f'{text_for_write}')
+
+        repeat_write_file = input('Створит ще файл? (y/n): ').lower()
+
+
+def read_file():
+    """
+    Вибираємо і читаємо файл.
+    """
+    repeat_read_file = 'y'
+    while repeat_read_file == 'y':
+        list_files = name_files()
+        my_choice = int(input('Виберіть номер файла для читання: '))
+
+        selected_file = list_files[my_choice]
+        with open(selected_file, 'r') as file:
+            output = file.read()
+            print(output)
+
+        repeat_read_file = input('Прочитати ще один файл? (y/n): ').lower()
+
+
+def main():
+    welcome = "Яку дію бажаєте виконати:\nВведіть 'r' - прочитати або " \
+              "'w' - редагувати/створити файл.\n"\
+              "Для виходу будь яку клавішу: "
+
+    repeat_main = 'y'
+    while repeat_main == 'y':
+        select_options = input(welcome)
+
+        if select_options == 'r':
+            read_file()
+        elif select_options == 'w':
+            write_file()
+        else:
+            break
+
+        repeat_main = input(select_options).lower()
+
+
+if __name__ == '__main__':
+    main()
