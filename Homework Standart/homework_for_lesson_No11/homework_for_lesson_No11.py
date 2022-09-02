7d5645747c99efce418ac1a614806f9270799be9
Андрій <andryhamorozyuk7@gmail.com>
2022-09-02 23:23:33 +0300

    Додав ДЗ 11 Task 1, 2 в окремій директорії.

--- Homework Standart/homework_for_lesson_No11/homework_for_lesson_No11.py
+++ Homework Standart/homework_for_lesson_No11/homework_for_lesson_No11.py
@@ -1,4 +1,5 @@
 import glob
+import json
 
 
 def print_name_files(path=''):
@@ -13,67 +14,91 @@ def print_name_files(path=''):
         print('Немає файлів')
         return 0
 
-    print('| №  |Назва файлу|\n+----+-----------+')
+    len_row = [len(i) for i in names_files]  # довжина назвів файлів
+    max_len_row = max(len_row) + 2  # довжина найдовшого файла + два пробіла
+    print('|  № ' + '| Назва файлу'.ljust(max_len_row), '|', '\n|----+'
+          + '-' * max_len_row + '|')
+
     num_key = 1
     for file in names_files:
         list_files[num_key] = file
-        print('|', str(num_key).rjust(2), '|', file.ljust(10), '|')
+        print('|', str(num_key).rjust(2), '|',
+              file.ljust(max_len_row - 1) + '|')
         num_key += 1
-    print('+----+-----------+')
+
+    print('|----+' + '-' * max_len_row + '|')
 
     return list_files
-
-
-def write_file():
-    """
-    Записуємо новий текстовий файл.
-    """
-    repeat_write_file = 'y'
-    while repeat_write_file == 'y':
-        file_name = input('Назва нового файлу: ')
-        text_for_write = input('Введіть текст, для запису: ')
-
-        with open(f'./{file_name}.txt', 'w') as my_file:
-            my_file.write(f'{text_for_write}')
-
-        repeat_write_file = input('Створит ще файл? (y/n): ').lower()
 
 
 def read_file():
     """
     Вибираємо і читаємо файл.
     """
-    repeat_read_file = 'y'
-    while repeat_read_file == 'y':
+    while True:
         list_files = print_name_files()
         if list_files == 0:
-            return
+            return print("Немає файлів для перегляду")
 
-        my_choice = int(input('Виберіть номер файла для читання: '))
+        try:
+            my_choice = int(input('Виберіть номер файла для читання: '))
+        except ValueError:
+            print('\n! Має бути передано число !\n')
+            continue
+
+        if my_choice > len(list_files):
+            print('\n! Немає такого номеру !\n')
+            continue
+
         selected_file = list_files[my_choice]
         with open(selected_file, 'r') as file:
             print(file.read())
 
-        repeat_read_file = input("Прочитати ще один файл? (y/n): ").lower()
+        if input(" (y) Прочитати ще один файл?: ").lower() != 'y':
+            break
+
+
+def write_file():
+    """
+    Записуємо новий текстовий файл.
+    """
+
+    while True:
+        file_name = input('Назва нового файлу: ')
+        text_for_write = input('Введіть текст, для запису: ')
+
+        with open(f'./{file_name}.txt', 'w') as my_file:
+            my_file.write(f'{text_for_write}')
+
+        if input(' (y) Створит ще файл?: ').lower() != 'y':
+            break
+
+
+def phone_book():
+    create_contact = json.dumps(
+        {
+            'name': 'Andrew',  # input('Назва: '),
+            'second_name': 'Moroziuk',  # input('Прізвище: '),
+            'phone': '0687701388',  # input('Номер тел.: '),
+            'city': 'Irpin'  # input('Назва міста: ')
+        }, indent=4)
 
 
 def main():
-    welcome = "Яку дію бажаєте виконати:\nВведіть 'r' - прочитати або " \
-              "'w' - редагувати/створити файл.\n"\
-              "Для виходу будь яку клавішу: "
-
-    repeat_main = 'y'
-    while repeat_main == 'y':
-        select_options = input(welcome)
+    while True:
+        select_options = input("\nВведіть 'r' - прочитати / " 
+                               "'w' - редагувати/створити файл / "
+                               "'f' - редагувати/створити файл. "
+                               "Для виходу будь яку клавішу:")
 
         if select_options == 'r':
             read_file()
-        if select_options == 'w':
+        elif select_options == 'w':
             write_file()
-        if select_options not in ('w', 'r'):
+        elif select_options == 'j':
+            phone_book()
+        else:
             break
-
-        repeat_main = input(select_options).lower()
 
 
 if __name__ == '__main__':
