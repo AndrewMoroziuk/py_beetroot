70ef1dfcc1037a6a1f2cbd56b02e0ef67aef1f7d
Андрій <andryhamorozyuk7@gmail.com>
2022-09-11 01:34:18 +0300

    Додав ДЗ #14

--- Homework Standart/homework_for_lesson_No14.py
+++ Homework Standart/homework_for_lesson_No14.py
@@ -1,23 +1,40 @@
-# Task 1
-def logger(func):
-    def inner(*args, **kwargs):
-        print(func.__name__, *args)
+# # Task 1
+# def logger(func):
+#     def inner(*args, **kwargs):
+#         print(func.__name__, *args)
+#
+#     return inner
+#
+#
+# @logger
+# def add(x, y):
+#     return x + y
+#
+#
+# @logger
+# def square_all(*args):
+#     return [arg ** 2 for arg in args]
+#
+#
+# add(5, 6)
+# square_all(16, 69)
+#
+
+# Task 2
+def stop_words(fun):
+    stopwords = ['pepsi', 'BMW']
+
+    def inner(name):
+        row = fun(name)
+        for word in stopwords:
+            row = row.replace(word, '***')
+        print(row)
     return inner
 
 
-@logger
-def add(x, y):
-    return x + y
+@stop_words
+def create_slogan(name):
+    return f"{name} drinks pepsi in his brand new BMW!"
 
 
-@logger
-def square_all(*args):
-    return [arg ** 2 for arg in args]
-
-
-add(5, 6)
-square_all(16, 69)
-
-
-# Task 2
-
+create_slogan('Andrew')
