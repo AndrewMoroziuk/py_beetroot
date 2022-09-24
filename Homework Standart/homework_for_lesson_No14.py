6139dbb124bb772725db9078489a7e75b9d6fec7
Андрій <andryhamorozyuk7@gmail.com>
2022-09-24 22:53:45 +0300

    Додав ДЗ #15

--- Homework Standart/homework_for_lesson_No14.py
+++ Homework Standart/homework_for_lesson_No14.py
@@ -1,23 +1,33 @@
-# Task 1
-def logger(func):
-    def inner(*args, **kwargs):
-        print(func.__name__, *args)
+# # Task 1
+# def logger(my_func):
+#     def inner(*args, **kwargs):
+#         print(my_func.__name__, my_func(*args, **kwargs))
+#         return my_func(*args, **kwargs)
+#
+#     return inner
+#
+#
+# @logger
+# def add(x, y):
+#     return x + y
+#
+#
+# @logger
+# def square_all(*args):
+#     return [arg ** 2 for arg in args]
+#
+#
+# add(3, 4)
+# square_all(2, 4, 8)
 
-    return inner
-
-
-@logger
-def add(x, y):
-    return x + y
-
-
-@logger
-def square_all(*args):
-    return [arg ** 2 for arg in args]
-
-
-add(5, 6)
-square_all(16, 69)
+#
+# @logger
+# def square_all(*args):
+#     return [arg ** 2 for arg in args]
+#
+#
+# add(5, 6)
+# square_all(16, 69)
 
 
 # Task 2
@@ -39,22 +49,22 @@ def create_slogan(name):
 
 
 create_slogan('Andrew')
-
-
-# Task 3
-def arg_rules(fun):
-    def inner(name):
-        row = fun(name)
-        if len(row) >= 15 and type(row) is str and '05' in row and '@' in row:
-            return row
-        return False
-
-    return inner
-
-
-@arg_rules
-def create_slogan(name):
-    return f"{name} drinks pepsi in his brand new BMW!"
-
-
-print(create_slogan('S@SH05'))
+#
+#
+# # Task 3
+# def arg_rules(fun):
+#     def inner(name):
+#         row = fun(name)
+#         if len(row) >= 15 and type(row) is str and '05' in row and '@' in row:
+#             return row
+#         return False
+#
+#     return inner
+#
+#
+# @arg_rules
+# def create_slogan(name):
+#     return f"{name} drinks pepsi in his brand new BMW!"
+#
+#
+# print(create_slogan('S@SH05'))
